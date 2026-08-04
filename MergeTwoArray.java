ed1063d808c57cf13f2c34f763db504ddc692fb2
bommisettiasha-create <bommisettiasha@gmail.com>
2026-08-04 21:28:22 +0530

    Format MergeTwoArray.java for consistency

--- MergeTwoArray.java
+++ MergeTwoArray.java
@@ -1,8 +1,8 @@
-import java.util.*;
- class Main{
- public static void main(String[] args){
- Scanner sc=new Scanner(System.in); 
-int n1=sc.nextInt();
+  import java.util.*;
+    class Main{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in); 
+   int n1=sc.nextInt();
  int[] a=new int[n1]; 
 for(int i=0;i<n1;i++) 
 a[i]=sc.nextInt(); 
