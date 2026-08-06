e07c9b44e7dd581d221a70680a7c26f2f336c4d7
bommisettiasha-create <bommisettiasha@gmail.com>
2026-08-06 20:50:58 +0530

    Format StringPalindrome.java code for consistency

--- StringPalindrome.java
+++ StringPalindrome.java
@@ -1,13 +1,11 @@
 public class StringPalindrome {
-    public static void main(String[] args) {
-        String str = "madam";
-        String rev = "";
-
-        for (int i = str.length() - 1; i >= 0; i--) {
+ public static void main(String[] args) {
+    String str = "madam";
+    String rev = "";
+ for (int i = str.length() - 1; i >= 0; i--) {
             rev += str.charAt(i);
         }
-
-        if (str.equals(rev))
+  if (str.equals(rev))
             System.out.println("Palindrome");
         else
             System.out.println("Not Palindrome");
