ff552ea0520856c24e47f446be7ee6683aa63d34
bommisettiasha-create <bommisettiasha@gmail.com>
2026-08-06 20:51:29 +0530

    Fix formatting issues in MultiplicationTable.java

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -1,9 +1,8 @@
 public class MultiplicationTable {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
         int n = 7;
-
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(n + " x " + i + " = " + (n * i));
+ for (int i = 1; i <= 10; i++) {
+ System.out.println(n + " x " + i + " = " + (n * i));
         }
     }
 }
