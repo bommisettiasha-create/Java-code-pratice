41df804be336fd3510c699b176c708e0aa944de2
bommisettiasha-create <bommisettiasha@gmail.com>
2026-08-11 18:44:02 +0530

    Refactor PerfectNumber class formatting

--- PerfectNumber.java
+++ PerfectNumber.java
@@ -1,17 +1,15 @@
 public class PerfectNumber {
-    public static void main(String[] args) {
-        int num = 28;
-        int sum = 0;
-
-        for (int i = 1; i < num; i++) {
-            if (num % i == 0) {
-                sum = sum + i;
+public static void main(String[] args) {
+int num = 28;
+ int sum = 0;
+  for (int i = 1; i < num; i++) {
+  if (num % i == 0) {
+    sum = sum + i;
             }
         }
-
-        if (sum == num)
-            System.out.println("Perfect Number");
-        else
-            System.out.println("Not a Perfect Number");
+  if (sum == num)
+  System.out.println("Perfect Number");
+  else
+ System.out.println("Not a Perfect Number");
     }
 }
