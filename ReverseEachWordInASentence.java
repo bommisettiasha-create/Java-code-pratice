50786913c8522a8d84c068cd7a7f3d69779fe0e7
bommisettiasha-create <bommisettiasha@gmail.com>
2026-08-20 19:37:09 +0530

    Refactor ReverseEachWordInASentence.java code

--- ReverseEachWordInASentence.java
+++ ReverseEachWordInASentence.java
@@ -1,17 +1,13 @@
 public class ReverseWords {
-    public static void main(String[] args) {
-        String str = "Java is easy";
-
-        String[] words = str.split(" ");
-
-        for (String word : words) {
-            String reverse = "";
-
-            for (int i = word.length() - 1; i >= 0; i--) {
-                reverse += word.charAt(i);
-            }
-
-            System.out.print(reverse + " ");
+public static void main(String[] args) {
+String str = "Java is easy";
+String[] words = str.split(" ");
+for (String word : words) {
+ String reverse = "";
+for (int i = word.length() - 1; i >= 0; i--) {
+  reverse += word.charAt(i);
+     }
+ System.out.print(reverse + " ");
         }
     }
 }
