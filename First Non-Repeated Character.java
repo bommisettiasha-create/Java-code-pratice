2dd4e05b0e9e52f707845dd2d25f7532481b5d76
bommisettiasha-create <bommisettiasha@gmail.com>
2026-08-20 19:32:18 +0530

    Add files via upload

---
+++ First Non-Repeated Character.java
@@ -0,0 +1,21 @@
+public class FirstNonRepeated {
+    public static void main(String[] args) {
+        String str = "aabbcde";
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            int count = 0;
+
+            for (int j = 0; j < str.length(); j++) {
+                if (ch == str.charAt(j)) {
+                    count++;
+                }
+            }
+
+            if (count == 1) {
+                System.out.println("First non-repeated = " + ch);
+                break;
+            }
+        }
+    }
+}
