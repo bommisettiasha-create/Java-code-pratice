24576a33db91e979c89dca98d4224804232366c2
bommisettiasha-create <bommisettiasha@gmail.com>
2026-08-21 19:26:37 +0530

    Fix formatting issues in DecimalToBinary.java

--- DecimalToBinary.java
+++ DecimalToBinary.java
@@ -1,15 +1,12 @@
 public class DecimalToBinary {
     public static void main(String[] args) {
-
-        int num = 10;
-        String binary = "";
-
-        while (num > 0) {
-            int remainder = num % 2;
-            binary = remainder + binary;
-            num = num / 2;
-        }
-
-        System.out.println("Binary = " + binary);
+int num = 10;
+String binary = "";
+while (num > 0) {
+ int remainder = num % 2;
+binary = remainder + binary;
+  num = num / 2;
+ }
+ System.out.println("Binary = " + binary);
     }
 }
