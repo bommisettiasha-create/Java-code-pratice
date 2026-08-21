5e62d522669fe51f5bcf9760619b7cee65c9a1f7
bommisettiasha-create <bommisettiasha@gmail.com>
2026-08-21 19:22:25 +0530

    Add files via upload

---
+++ DecimalToBinary.java
@@ -0,0 +1,15 @@
+public class DecimalToBinary {
+    public static void main(String[] args) {
+
+        int num = 10;
+        String binary = "";
+
+        while (num > 0) {
+            int remainder = num % 2;
+            binary = remainder + binary;
+            num = num / 2;
+        }
+
+        System.out.println("Binary = " + binary);
+    }
+}
