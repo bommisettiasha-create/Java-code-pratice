25e498be21e1a8f150df19d01fc04e7ca740f9cc
bommisettiasha-create <bommisettiasha@gmail.com>
2026-08-24 21:00:19 +0530

    Fix formatting and clean up ProductOfDigits.java

--- ProductOfDigits.java
+++ ProductOfDigits.java
@@ -1,15 +1,12 @@
 public class ProductDigits {
     public static void main(String[] args) {
-
-        int num = 1234;
+int num = 1234;
         int product = 1;
-
         while (num != 0) {
             int digit = num % 10;
             product = product * digit;
             num = num / 10;
         }
-
-        System.out.println("Product = " + product);
+ System.out.println("Product = " + product);
     }
 }
